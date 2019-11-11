5436f4bf6152789d0f7b272e75c3de045790fcd8
KevinGallet <kevingallet@gmail.com>
2019-11-11 17:06:44 +0100

    Init

---
+++ README.md
@@ -0,0 +1,26 @@
+Simple script to download all pictures from a facebook conversation
+This script is using [fbchat > 1.8.0](https://github.com/carpedm20/fbchat)
+
+# Usage
+
+Don't forget to change DESTINATION_FOLDER variable.
+
+```
+usage: download_messenger_pictures.py [-h] [--output OUTPUT] 
+                                      email password thread_id
+
+positional arguments:
+  email                 Email of your facebook account
+  password              Password of your facebook account
+  thread_id             ID of your conversation
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --output OUTPUT, -o OUTPUT
+                        Output folder where pictures will be stored
+```
+
+To use it in a crontab, for example : 
+```
+0 20 * * * sudo /usr/bin/python3.7 download_messenger_pictures.py <email> <password> <thread_id> --output <destination_folder> > <destination_folder>/logs.info 2>&1 & 
+```
---
+++ download_messenger_pictures.py
@@ -0,0 +1,60 @@
+#!python3
+import os
+from os import mkdir
+from os.path import isdir, join
+from datetime import datetime
+
+from fbchat import Client
+from fbchat.models import ImageAttachment
+import urllib3
+
+http = urllib3.PoolManager()
+
+DESTINATION_FOLDER="XXX"
+
+def isFileExist(filename):
+    for dir, sub_dirs, files in os.walk(DESTINATION_FOLDER):
+        if filename in files:
+            return True
+    return False
+
+def download_pictures(user, password, thread_id, output_folder):
+    downloaded = 0
+    if not isdir(output_folder):
+        mkdir(output_folder)
+
+    client = Client(user, password)
+
+    images = client.fetchThreadImages(thread_id)
+    for im in images:
+        if not type(im) == ImageAttachment:
+            continue
+        if (im.original_extension) == 'gif':
+            continue
+        output_filename = join(output_folder, im.uid + ".jpg")
+        if (isFileExist(output_filename)):
+            print("File %s already downloaded" % output_filename)
+            continue
+        url = client.fetchImageUrl(im.uid)
+        data = http.request("GET", url=url, preload_content=False)
+        data = data.read()
+        downloaded += 1
+        print("Downloading %s" % output_filename)
+
+        with open(output_filename, "wb") as f:
+            f.write(data)
+
+    client.send(Message(text="Synchronisation du cloud fini, nb photos telecharges: " + downloaded), thread_id=client.uid, thread_type=ThreadType.USER)
+    client.send(Message(text="Synchronisation du cloud fini, nb photos telecharges: " + downloaded), thread_id=thread_id, thread_type=ThreadType.USER)
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("email", help="Email of your facebook account")
+    parser.add_argument("password", help="Password of your facebook account")
+    parser.add_argument("thread_id", help="ID of your conversation")
+    parser.add_argument("--output", '-o', help="Output folder where pictures will be stored", default=".")
+
+    args = parser.parse_args()
+    download_pictures(args.email, args.password, args.thread_id, args.output)
